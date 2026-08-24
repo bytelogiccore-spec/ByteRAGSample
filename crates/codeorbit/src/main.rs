9477db098cd94521011e71a343de43b26f343e91
ByteLogicCore <bytelogiccore@gmail.com>
2026-08-24 21:47:05 +0900

    Hide black console window permanently on launch using unconditional windows subsystem

--- crates/codeorbit/src/main.rs
+++ crates/codeorbit/src/main.rs
@@ -1,5 +1,5 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// Prevents black console window from appearing on Windows desktop launch
+#![windows_subsystem = "windows"]
 
 pub mod commands;
 pub mod config;
